114ca37ad07ded40bafe089cc80cf71a56211133
Rija Ménagé <git@rijam.sent.as>
2016-06-02 16:45:56 +0800

    both wordpress and formidable form db wrappers passed to our class' constructor

--- src/classes/FF_Timeslot_Booking.php
+++ src/classes/FF_Timeslot_Booking.php
@@ -2,9 +2,19 @@
 class FF_Timeslot_Booking {
 
   private $_wpdb;
+  private $_frmdb;
 
-  public function __construct($wpdb) {
+  public function __construct($wpdb,$frmdb) {
       $this->_wpdb = $wpdb;
+      $this->_frmdb = $frmdb;
+  }
+
+  public function get_wpdb () {
+    return $this->_wpdb;
+  }
+
+  public function get_frmdb () {
+    return $this->_frmdb;
   }
 
   public function update_session_booked_field ($entry_id, $form_id) {
--- test/test-ff-timeslot-booking.php
+++ test/test-ff-timeslot-booking.php
@@ -1,8 +1,5 @@
 <?php
 
-/**
- * An example test case.
- */
 class FF_Timeslot_Booking_Test extends PHPUnit_Framework_TestCase {
 
     public function setUp() {
@@ -14,19 +11,56 @@ class FF_Timeslot_Booking_Test extends PHPUnit_Framework_TestCase {
     }
 
 
-    function test_booked_field_in_session_is_updated () {
+    function test_class_can_be_instantiated () {
       $wpdb = new \stdClass;
-      $tsb = new FF_Timeslot_Booking ($wpdb) ;
+      $frmdb = new \stdClass;
+      $tsb = new FF_Timeslot_Booking ($wpdb, $frmdb) ;
+
+      $this->assertNotNull($tsb->get_wpdb());
+      $this->assertNotNull($tsb->get_frmdb());
+    }
+
+    function test_action_verifies_form_parameter () {
+      $wpdb = new \stdClass;
+      $frmdb = new \stdClass;
+      $tsb = new FF_Timeslot_Booking ($wpdb, $frmdb) ;
 
       // return something true if right form
       $form_id = 2;
       $this->assertTrue($tsb->update_session_booked_field(null, $form_id));
 
-      // return undef if not the right Formidable
+      // return undef if not the right form
       $form_id = 1000 ;
       $this->assertNull($tsb->update_session_booked_field(null, $form_id));
 
+    }
 
+    function disabled_test_booked_field_in_session_is_updated () {
+
+      $_POST['item_meta'] = array( 103 => 71) ;
+
+      $wpdb = $this->getMockBuilder('Db')
+                 ->setMethods(array('update'))
+                 ->getMock();
+
+      $frmdb = $this->getMockBuilder('FrmDb')
+                 ->setMethods(array('item_metas'))
+                 ->getMock();
+
+      $tsb = new FF_Timeslot_Booking ($wpdb) ;
+
+      $item_metas = [] ;
+      $frmdb->expects($this->once())
+            ->method('item_metas')
+            ->willReturn($item_metas);
+
+      $wpdb->expects($this->once())
+            ->method('update')
+            ->with(
+            $item_metas,
+            array($this->equalTo('meta_value'), "Yes" ),
+            array($this->equalTo('item_id'), 71 )
+            ) ;
     }
 
 
