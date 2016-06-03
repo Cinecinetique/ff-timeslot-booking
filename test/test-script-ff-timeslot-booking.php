16142b20e5aab4a671a1475168692d666ce005bb
Rija Ménagé <git@rijam.sent.as>
2016-06-03 21:21:00 +0800

    tested integration with Wordpress API. Cleaned up dependencies and autoloading (missed the test file)

---
+++ test/test-script-ff-timeslot-booking.php
@@ -0,0 +1,27 @@
+<?php
+
+class Script_FF_Timeslot_Booking_Test extends PHPUnit_Framework_TestCase {
+
+  public function setUp() {
+        WP_Mock::setUp();
+  }
+
+  public function tearDown() {
+        WP_Mock::tearDown();
+  }
+
+  function test_action_hook_is_created () {
+    $wpdb = new stdClass;
+    $frmdb = new stdClass;
+
+    $main = new FF_Timeslot_Booking_Main () ;
+
+    WP_Mock::expectActionAdded( 'frm_after_create_entry',
+                                  array ($this->isInstanceOf ('FF_Timeslot_Booking'), 'update_session_booked_field') ,
+                                  30, 2
+                                );
+
+    $main->register_ff_timeslot_booking($wpdb, $frmdb);
+  }
+
+}
