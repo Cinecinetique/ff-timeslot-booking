b86978f2a3c4627ceae11da2d7491b2c7f6f1559
Rija Ménagé <git@rijam.sent.as>
2016-06-03 22:12:37 +0800

    the email of the person who register for a time slot is also save in the session being booked

--- src/classes/class-ff-timeslot-booking.php
+++ src/classes/class-ff-timeslot-booking.php
@@ -1,9 +1,11 @@
 <?php
 class FF_Timeslot_Booking {
 
-  const SESSION_BOOKED_FIELD_ID = 44 ;
   const BOOKING_FORM_ID = 2 ;
   const SESSION_FIELD_ID_IN_BOOKING_FORM = 103 ;
+  const EMAIL_FIELD_ID_IN_BOOKING_FORM = 13 ;
+  const BOOKED_FIELD_ID_IN_SESSION_FORM = 44 ;
+  const EMAIL_FIELD_ID_IN_SESSION_FORM = 102 ;
 
   private $_wpdb;
   private $_frmdb;
@@ -23,14 +25,25 @@ class FF_Timeslot_Booking {
 
   public function update_session_booked_field ($entry_id, $form_id) {
 
+    $session_id = $_POST['item_meta'][self::SESSION_FIELD_ID_IN_BOOKING_FORM];
+    $client_email = $_POST['item_meta'][self::EMAIL_FIELD_ID_IN_BOOKING_FORM];
+
     if ( self::BOOKING_FORM_ID == $form_id && $this->_wpdb && $this->_frmdb) {
 
-      $session_id = $_POST['item_meta'][self::SESSION_FIELD_ID_IN_BOOKING_FORM];
+
       $this->_wpdb->update(
         $this->_frmdb->entry_metas,
         array ( 'meta_value' => 'Yes' ),
-        array ( 'item_id' => $session_id, 'field_id' => self::SESSION_BOOKED_FIELD_ID )
+        array ( 'item_id' => $session_id, 'field_id' => self::BOOKED_FIELD_ID_IN_SESSION_FORM )
       );
+
+      $this->_wpdb->update(
+        $this->_frmdb->entry_metas,
+        array ( 'meta_value' => $client_email ),
+        array ( 'item_id' => $session_id, 'field_id' => self::EMAIL_FIELD_ID_IN_SESSION_FORM )
+      );
+
+
       return true;
     }
 
--- test/test-class-ff-timeslot-booking.php
+++ test/test-class-ff-timeslot-booking.php
@@ -5,18 +5,26 @@ use \Mockery as m;
 class FF_Timeslot_Booking_Test extends PHPUnit_Framework_TestCase {
 
     private $session_field_id_in_booking_form ;
+    private $email_field_id_in_booking_form ;
     private $booked_field_id_in_session_form ;
+    private $email_field_id_in_session_form ;
     private $fake_session_id ;
+    private $fake_email ;
 
     public function setUp() {
           $session_field_id_in_booking_form = 103 ;
           $booked_field_id_in_session_form = 44 ;
+          $email_field_id_in_booking_form = 13 ;
           $fake_session_id = 71 ;
-          $_POST['item_meta'] = array ( $session_field_id_in_booking_form => $fake_session_id) ;
+          $fake_email = "foo@bar.com" ;
+          $_POST['item_meta'] = array (
+            $session_field_id_in_booking_form => $fake_session_id,
+            $email_field_id_in_booking_form => $fake_email
+          ) ;
     }
 
     public function tearDown() {
-      m::close();
+      m::close() ;
     }
 
 
@@ -25,15 +33,15 @@ class FF_Timeslot_Booking_Test extends PHPUnit_Framework_TestCase {
       $frmdb = new stdClass;
       $tsb = new FF_Timeslot_Booking ($wpdb, $frmdb) ;
 
-      $this->assertNotNull($tsb->get_wpdb());
-      $this->assertNotNull($tsb->get_frmdb());
+      $this->assertNotNull($tsb->get_wpdb()) ;
+      $this->assertNotNull($tsb->get_frmdb()) ;
     }
 
     function test_action_verifies_form_parameter () {
-      $wpdb = m::mock ('wpdb');
-      $frmdb = new stdClass;
+      $wpdb = m::mock ('wpdb') ;
+      $frmdb = new stdClass ;
 
-      $wpdb->shouldReceive('update');
+      $wpdb->shouldReceive('update') ;
 
       $frmdb->entry_metas = array (1,2) ;
 
@@ -41,11 +49,11 @@ class FF_Timeslot_Booking_Test extends PHPUnit_Framework_TestCase {
 
       // return something true if right form
       $form_id = 2;
-      $this->assertTrue($tsb->update_session_booked_field(null, $form_id));
+      $this->assertTrue($tsb->update_session_booked_field(null, $form_id)) ;
 
       // return undef if not the right form
       $form_id = 1000 ;
-      $this->assertNull($tsb->update_session_booked_field(null, $form_id));
+      $this->assertNull($tsb->update_session_booked_field(null, $form_id)) ;
 
     }
 
@@ -53,11 +61,13 @@ class FF_Timeslot_Booking_Test extends PHPUnit_Framework_TestCase {
 
       $fake_session_id = 71 ;
       $booked_field_id_in_session_form = 44 ;
+      $email_field_id_in_session_form = 102 ;
       $entry_metas = 1 ;
+      $client_email = "foo@bar.com";
       $form_id = 2 ;
 
-      $wpdb = m::mock('wpdb');
-      $frmdb = new stdClass;
+      $wpdb = m::mock('wpdb') ;
+      $frmdb = new stdClass ;
 
       $frmdb->entry_metas = $entry_metas ;
 
@@ -68,7 +78,16 @@ class FF_Timeslot_Booking_Test extends PHPUnit_Framework_TestCase {
               $entry_metas,
               array ('meta_value' => 'Yes' ),
               array ('item_id' => $fake_session_id , 'field_id' => $booked_field_id_in_session_form )
-            ) ;
+            )
+            ->times(1) ;
+
+      $wpdb->shouldReceive('update')
+            ->with(
+              $entry_metas,
+              array ('meta_value' => $client_email ),
+              array ('item_id' => $fake_session_id , 'field_id' => $email_field_id_in_session_form )
+            )
+            ->times(1) ;
 
       $tsb->update_session_booked_field(null, $form_id) ;
 
